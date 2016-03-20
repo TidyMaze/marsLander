18e58d423395dbc32b4a2bf52e06f3ef32a1396a
TidyMaze <GrosKiff@gmail.com>
2016-03-20 01:35:00 +0100

    bruteforce

--- marslander.py
+++ marslander.py
@@ -4,6 +4,7 @@ import math
 import random
 import time
 import cProfile
+import threading
 
 width = 1200
 height = 700
@@ -11,6 +12,11 @@ GRAV = 3.711
 POD_RADIUS = 5
 MAX_VSPEED_LANDING = 40
 MAX_HSPEED_LANDING = 20
+
+minX = width
+maxX = 0
+minY = height
+maxY = 0
 
 class State:
     def __init__(self):
@@ -34,7 +40,7 @@ class Pod:
         self.power = 0
         self.fuel = 0
     def __str__(self):
-        return 'position : ' + str(self.position) + ', speed : ' + str(self.speed) + ', angle : ' + str(self.angle) + ', fuel : ' + str(self.fuel)
+        return 'position : ' + str(self.position) + ', speed : ' + str(self.speed) + ', angle : ' + str(self.angle) + ', power : ' + str(self.power) + ', fuel : ' + str(self.fuel)
 
 class Vector:
     def __init__(self, x=0, y=0):
@@ -120,21 +126,6 @@ def intersectionSegments(s1, s2):
         return False
     return inRangeSorted(pInter[0], s1.p1.x, s1.p2.x) and inRangeSorted(pInter[0], s2.p1.x, s2.p2.x) and inRangeSorted(pInter[1], s1.p1.y, s1.p2.y) and inRangeSorted(pInter[1], s2.p1.y, s2.p2.y)
 
-def updateGame(state, angle, power):
-    vectorThrust = Vector(math.cos(math.radians(angle))*power, math.sin(math.radians(angle))*power)
-    #print('thrust acc : ', vectorThrust)
-    acceleration = Vector(0, -GRAV).add(vectorThrust)
-    newState = State()
-    newState.pod = Pod()
-    newState.pod.angle = angle
-    newState.pod.power = power
-    newState.pod.speed = state.pod.speed.add(acceleration)
-    newState.pod.position = state.pod.position.apply(newState.pod.speed)
-    newState.pod.fuel = state.pod.fuel - power
-    newState.history = list(state.history)
-    newState.history.append((angle,power))
-    return newState
-
 def isFlat(line):
     return line.p1.y == line.p2.y
 
@@ -146,10 +137,6 @@ def lastMoveInCollisionWithNotFlatLine(state, line):
     last = lastMove(pod)
     inter = intersectionSegments(last,line)
     coliNotFlat = inter and not isFlat(line)
-    """
-    if coliNotFlat:
-        print('collision entre ', lastMove, ' and ', line)
-    """
     return coliNotFlat
 
 def lastMoveInCollisionWithFlatLine(state, line):
@@ -157,9 +144,6 @@ def lastMoveInCollisionWithFlatLine(state, line):
     last = lastMove(pod)
     inter = intersectionSegments(last,line)
     coliFlat = inter and isFlat(line)
-    if coliFlat:
-        print('.',end='')
-        #print('collision flat entre ', lastMove, ' and ', line)
     return coliFlat
 
 def empty(list):
@@ -169,6 +153,7 @@ def collisionWithNotFlatMountain(state, mountainLines):
     functionLastMoveCollision = lambda line : lastMoveInCollisionWithNotFlatLine(state, line)
     for line in mountainLines:
         if functionLastMoveCollision(line):
+            #print('crashed :( in' + str(line) + ' while in ' + str(state.pod))
             return True
     return False
 
@@ -185,7 +170,8 @@ def inBoundaries(point):
 
 def lost(state, mountainLines):
     return not inBoundaries(state.pod.position) \
-        or collisionWithNotFlatMountain(state, mountainLines)
+        or collisionWithNotFlatMountain(state, mountainLines) \
+        or (collisionWithFlatMountain(state, mountainLines) and not podSpeedCanLand(state.pod))
 
 def podSpeedCanLand(pod):
     return abs(pod.speed.y) <= MAX_VSPEED_LANDING \
@@ -196,21 +182,22 @@ def win(state, mountainLines):
         collisionWithFlatMountain(state, mountainLines)# \
         #and round(state.pod.angle) == 90
 
+def dst(p1, p2):
+    return math.sqrt(math.pow(p2.x-p1.x,2)+math.pow(p2.y-p1.y,2))
+
 def showMove(canvas, state, pod, mountainLines):
-    #canvas.delete("moving")
     sTemp = State()
     sTemp.pod = pod
     path = []
     index = 0
     while True :
-        #print('index:',index,', history:', state.history)
         sTemp = updateGame(sTemp, state.history[index][0], state.history[index][1])
         path.append((sTemp.pod.position.x, sTemp.pod.position.y))
 
         if podSpeedCanLand(sTemp.pod):
             drawPod(canvas,sTemp.pod, 'green')
         else:
-            """drawPod(canvas,sTemp.pod, 'red')"""
+            drawPod(canvas,sTemp.pod, 'red')
         index += 1
         if lost(sTemp, mountainLines) or win(sTemp, mountainLines):
             break;
@@ -218,16 +205,74 @@ def showMove(canvas, state, pod, mountainLines):
     drawPod(canvas,state.pod, 'white')
     drawMultiLines(canvas, coordsToLines(path), 'blue')
 
-def startSimu(pod, mountainLines):
-    s = State()
-    s.pod = pod
+def updateGame(state, angle, power):
+    #print('updating : before : ' + str(state))
+    vectorThrust = Vector(math.cos(math.radians(angle))*power, math.sin(math.radians(angle))*power)
+    acceleration = Vector(0, -GRAV).add(vectorThrust)
+    newState = State()
+    newState.pod = Pod()
+    newState.pod.angle = angle
+    newState.pod.power = power
+    newState.pod.speed = state.pod.speed.add(acceleration)
+    newState.pod.position = state.pod.position.apply(newState.pod.speed)
+    newState.pod.fuel = state.pod.fuel - power
+    newState.history = list(state.history)
+    newState.history.append((angle,power))
+    #print('updating : after : ' + str(newState))
+    global minX
+    global maxX
+    global minY
+    global maxY
 
-    while not lost(s, mountainLines) and not win(s, mountainLines):
-        power = min(s.pod.fuel,random.randint(max(0,s.pod.power-1),min(s.pod.power+1,4)))
-        angle = min(180,max(0, random.randint(s.pod.angle-15,s.pod.angle+15)))
-        s = updateGame(s, angle, power)
+    def writeMax():
+        print(str(minX) + '-' + str(maxX) + ', ' + str(minY) + '-' + str(maxY))
 
-    return (s, win(s, mountainLines))
+    if newState.pod.position.x < minX:
+        minX = newState.pod.position.x
+        writeMax()
+    if newState.pod.position.x > maxX:
+        maxX = newState.pod.position.x
+        writeMax()
+    if newState.pod.position.y < minY:
+        minY = newState.pod.position.y
+        writeMax()
+    if newState.pod.position.y > maxY:
+        maxY = newState.pod.position.y
+        writeMax()
+    return newState
+
+def findWinningActions(state, mountainLines, canvas, initPod):
+    #print('findWinningActions of ' + str(state.pod))
+    if lost(state, mountainLines):
+        showMove(canvas, state, initPod, mountainLines)
+        #print(':(')
+        return None
+    if win(state, mountainLines):
+        print('win ! :)')
+        return state
+
+    minPower = max(0,state.pod.power-1)
+    maxPower = min(state.pod.fuel,min(state.pod.power+1,4))
+    #minAngle = max(0,state.pod.angle-15)
+    #maxAngle = min(180,state.pod.angle+15)
+
+    nextStates = []
+    for power in range(maxPower, minPower, -1):
+        #for angle in range(minAngle, maxAngle):
+
+        if state.pod.fuel > 0:
+            angles = filter(lambda x: x>=0 and x<=180, [state.pod.angle + 15, state.pod.angle, state.pod.angle - 15])
+        else:
+            angles = [state.pod.angle]
+        for angle in angles:
+            nextStates.append(updateGame(state, angle, power))
+
+    nextStates.sort(key=lambda x: dst(x.pod.position, Point(840,300)))
+    for nextState in nextStates:
+        winActions = findWinningActions(nextState, mountainLines, canvas, initPod)
+        if winActions is not None:
+            return winActions
+    return None
 
 def computeCoordinates(relief):
     nbPoints = len(relief)
@@ -242,18 +287,20 @@ def main():
 def findAndShow(mountainLines, canvas):
     found=0
     pod = Pod()
-    pod.position = Point(300, 300)
+    pod.position = Point(300, 500)
     pod.speed = Vector(0,0)
     pod.angle = 90
-    pod.fuel = 500
-    pod.power = 3
-    while found<1:
-        res = startSimu(pod, mountainLines)
-        s = res[0]
-        if res[1]:
-            found += 1
-            print('final state : ', s)
-            showMove(canvas, s, pod, mountainLines)
+    pod.fuel = 50
+    pod.power = 0
+
+    state = State()
+    state.pod = pod
+    finalState = findWinningActions(state, mountainLines, canvas, pod)
+    if finalState is not None:
+        print('final state : ', finalState)
+        showMove(canvas, finalState, pod, mountainLines)
+    else:
+        print('Nothing found :(')
 
 
 def createWindow(coordinates):
@@ -263,7 +310,9 @@ def createWindow(coordinates):
     canvas.pack()
     mountainLines = coordsToLines(coordinates)
     start_time = time.time()
-    findAndShow(mountainLines, canvas)
+    thread = threading.Thread(target = findAndShow, args = (mountainLines, canvas))
+    thread.start()
+    #findAndShow(mountainLines, canvas)
     print('')
     print('time : ', time.time() - start_time)
     top.mainloop()
